453c0111a76610ec17b01246b9bd863098437c5d
Joonkyo Kim <rootwarp@gmail.com>
2022-07-21 23:33:32 +0900

    Change favicon.

--- frontend/wenmerge/src/App.js
+++ frontend/wenmerge/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 
 import BlockInfo from './components/blockInfo.js';
-import TBD from './components/tbd.js';
 
 function App() {
   return (
-    <div className="App">
-      <h className='App__title'>wenmerge?</h>
+    <div className="app">
+      <div className="app__title_container">
+        <p className='app__title'>wenmerge?</p>
+        <p className='app__from'>by DSRV</p>
+        <a href='https://dsrvlabs.com' className='app__home_link'>> GO HOME</a>
+      </div>
       <BlockInfo title="GSF5 - goerli shadow fork" target_ttd="10625003" rpc="https://api-wenmerge-goerli.dsrvlabs.dev"></BlockInfo>
       <BlockInfo title="goerli merge" target_ttd="10790000" rpc="https://api-wenmerge-goerli.dsrvlabs.dev"></BlockInfo>
-      <TBD title="mainnet"></TBD>
     </div>
   );
 }
