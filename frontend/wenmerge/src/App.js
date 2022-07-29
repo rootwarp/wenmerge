039cbeee3cbfae6aa4efe6aee5719f9bdd2929d1
Joonkyo Kim <rootwarp@gmail.com>
2022-07-29 16:26:24 +0900

    Remove MSF10

--- frontend/wenmerge/src/App.js
+++ frontend/wenmerge/src/App.js
@@ -32,7 +32,6 @@ function App() {
         <p className='app__from'>by DSRV</p>
         <a href='https://dsrvlabs.com' className='app__home_link'>> GO HOME</a>
       </div>
-      <BlockInfo title="MSF10 - Mainnet shasow fork" target_ttd="54892060024013871644672" rpc="https://api-wenmerge.dsrvlabs.dev"></BlockInfo>
       <BlockInfo title="goerli merge" target_ttd="10790000" rpc="https://api-wenmerge-goerli.dsrvlabs.dev"></BlockInfo>
     </div>
   );
